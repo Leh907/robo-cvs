490dd4285649e9581ab7ada589eba22aea7fcf7f
Leh907 <114775232+Leh907@users.noreply.github.com>
2024-11-14 11:50:47 -0300

    robo-cvs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using SeleniumBot;
+using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var web = new AutomationWeb();
+        web.testWeb();
+    }
+}
